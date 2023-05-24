dcfb38bf7ee6527df982fdaf1c905f21f52250a3
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-24 17:09:34 +0600

    login and signup page created

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title text-center fw-bold'>Login</h2>
+            <form className='p-2'>
+                <div className=''>
+                    <label htmlFor="">Email</label>
+                    <input className='form-control' type="email" id='email' name='email' required />
+                </div>
+                <div className=''>
+                    <label htmlFor="">Password</label>
+                    <input className='form-control' type="password" id='password' name='password' required />
+                </div>
+                <input type="submit" value="Login" className='btn w-100 btn-outline-success mt-5 text-center' />
+
+            </form>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,8 +3,28 @@ import './SignUp.css';
 
 const SignUp = () => {
     return (
-        <div>
-            xx
+        <div className='form-container'>
+            <h2 className='form-title text-center fw-bold'>Login</h2>
+            <form className='p-2'>
+                <div className=''>
+                    <label htmlFor="">Name</label>
+                    <input className='form-control' type="text" id='name' name='name' required />
+                </div>
+                <div className=''>
+                    <label htmlFor="">Email</label>
+                    <input className='form-control' type="email" id='email' name='email' required />
+                </div>
+                <div className=''>
+                    <label htmlFor="">Password</label>
+                    <input className='form-control' type="password" id='password' name='password' required />
+                </div>
+                <div className=''>
+                    <label htmlFor="">Confirm Password</label>
+                    <input className='form-control' type="password" id='confirm' name='confirm' required />
+                </div>
+                <input type="submit" value="Sign Up" className='btn w-100 btn-outline-warning mt-5 text-center' />
+
+            </form>
         </div>
     );
 };
